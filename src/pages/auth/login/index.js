e8ae6396524f5e70495b7de4fc88e46e71f1087e
khachikkar <khachikkarapetyan78@gmail.com>
2024-10-25 00:22:28 +0400

    added loading functionality in login and updated zoom in issue on mobile

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AuthWraper from "../../../AuthWraper";
 
 import { Form, Button, Input, Flex, notification } from "antd";
@@ -12,10 +12,11 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 const Login = () => {
   const [form] = Form.useForm();
   const {setIsAuth} = useContext(Context)
-
+  const [loading, setLoading] = useState(false)
 
 
   const handleLogin = async(values) => {
+    setLoading(true)
     try {
         const { email, password } = values;
         await signInWithEmailAndPassword(auth, email, password);
@@ -26,7 +27,9 @@ const Login = () => {
         notification.error({
           message: "Invalid Login Credentials", // Fixed message typo
         });
-      } 
+      } finally{
+        setLoading(false)
+      }
     };
   
 
@@ -52,7 +55,7 @@ const Login = () => {
         >
           <Input
             type="email"
-            placeholder="email"
+            placeholder="Email"
             style={{ fontSize: "16px" }}
           />
         </Form.Item>
@@ -67,7 +70,7 @@ const Login = () => {
             },
           ]}
         >
-          <Input.Password placeholder="Password" />
+          <Input.Password placeholder="Password" style={{ fontSize: '16px' }} />
         </Form.Item>
 
         <Flex wrap justify="center" align="center" gap="10px">
@@ -76,7 +79,7 @@ const Login = () => {
             style={{ width: "100%" }}
             type="primary"
             htmlType="submit"
-            //   loading={loading}
+            loading={loading}
           >
             Login
           </Button>
